41780cd8c7d1789eec0154782e54b57d611f4c2a
Jan Denningmann <jan.denningmann@lvdigital.de>
2023-03-14 12:27:21 +0100

    add triggers to update availability of a book

--- db-skripte/trigger.sql
+++ db-skripte/trigger.sql
@@ -1,5 +1,6 @@
+-- check if a boook can be lent and update available
 DELIMITER //
-CREATE TRIGGER tr_validate_max_lended_books
+CREATE TRIGGER tr_book_can_be_lent
 	BEFORE INSERT ON lending
 	FOR EACH ROW IF(
 		SELECT
@@ -8,5 +9,33 @@ CREATE TRIGGER tr_validate_max_lended_books
 		NEW.u_id = lending.u_id) >= 5 THEN
 		SIGNAL SQLSTATE '45000'
 		SET MESSAGE_TEXT = 'Du darfst keine Bücher mehr ausleihen';
-		END IF//
+ELSEIF(
+		SELECT
+			book.is_available FROM book
+		WHERE
+			book.b_id = NEW.b_id) = 0 THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Das Buch ist bereits verliehen';
+ELSE
+	UPDATE
+		book
+	SET
+		book.is_available = 0
+	WHERE
+		book.b_id = NEW.b_id;
+END IF//
+DELIMITER ;
+
+-- update available after returning a book
+DELIMITER //
+CREATE TRIGGER tr_book_returned
+	AFTER UPDATE ON lending
+	FOR EACH ROW IF(ISNULL(OLD.return_date)) THEN
+	UPDATE
+		book
+	SET
+		book.is_available = 1
+	WHERE
+		book.b_id = NEW.b_id;
+END IF//
 DELIMITER ;
