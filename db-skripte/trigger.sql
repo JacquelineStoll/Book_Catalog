17e78feabdc71a0c4d6d2feb35b6d5d759fe8bfd
Jan Denningmann <jan.denningmann@lvdigital.de>
2023-03-14 08:43:41 +0100

    add trigger

---
+++ db-skripte/trigger.sql
@@ -0,0 +1,12 @@
+DELIMITER //
+CREATE TRIGGER tr_validate_max_lended_books
+	BEFORE INSERT ON lending
+	FOR EACH ROW IF(
+		SELECT
+			COUNT(*) FROM lending
+	WHERE
+		NEW.c_id = lending.c_id) >= 5 THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Du darfst keine Bücher mehr ausleihen';
+		END IF//
+DELIMITER ;
